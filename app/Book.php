9c737ba7a43c19bba33f605c274e6bcfd3e76e11
Azzahrah N A <zahrahalifah9@gmail.com>
2020-09-03 11:58:58 +0700

    challenge

---
+++ app/Book.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title', 'description', 'author', 'publisher', 'date_of_issue'
+    ];
+    public $timestamps=false;
+    
+    //
+}
